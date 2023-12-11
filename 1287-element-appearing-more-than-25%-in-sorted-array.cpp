4ebdb75ad7e471d1edfe1e559aebeb8b4e0614b9
Yuvraj Sharma <yuvraj.june2002@gmail.com>
2023-12-11 21:13:44 +0530

    Create 1287-element-appearing-more-than-25%-in-sorted-array.cpp

---
+++ 1287-element-appearing-more-than-25%-in-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        int freq = n/4;
+        
+        for(int i = 0; i<n-freq; i++)
+        {
+            if(arr[i] == arr[i+freq])
+                return arr[i];
+        }
+        
+        return -1;
+    }
+};
